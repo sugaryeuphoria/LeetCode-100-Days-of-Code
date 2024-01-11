b5f948e96a299c2dea3c76ec8afeb8e2d38780c4
Pooja Verma <pooja32verma@gmail.com>
2024-01-10 21:05:10 -0800

    if condition 1

--- Day4.py
+++ Day4.py
@@ -2,4 +2,7 @@
 class Solution(object):
      # Define a method named longestCommonPrefix that takes a list of strings as input
     def longestCommonPrefix(self, strs):
-       
+        # Check if the list of strings is empty
+        if not strs:
+          
+    
