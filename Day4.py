c4833ea8ae903b2bbeb510a535f02c28b070e6f3
Pooja Verma <pooja32verma@gmail.com>
2024-01-10 21:04:06 -0800

    Method definition

--- Day4.py
+++ Day4.py
@@ -1,3 +1,5 @@
-#Class created
+#lass created
 class Solution(object):
-    
+     # Define a method named longestCommonPrefix that takes a list of strings as input
+    def longestCommonPrefix(self, strs):
+       
