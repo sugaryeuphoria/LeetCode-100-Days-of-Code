d28b4ca0b5998b301c25635f1efa45dad8bb3c28
Pooja Verma <pooja32verma@gmail.com>
2024-01-11 12:55:47 -0800

    dictionary

--- Day5.py
+++ Day5.py
@@ -2,4 +2,8 @@ class Solution(object):
     def isValid(self,s):
         #create an empty stack to keep a track of open brackets
         stack=[]
-       
+        
+        #define a dictionary to store the mapping of brackets
+        bracket_mapping = {')': '(', '}': '{', ']': '['}
+
+      
