101e685a998f4ffd6ebf811bb34328233f11f5d1
Pooja Verma <pooja32verma@gmail.com>
2024-01-12 22:34:30 -0800

    mergeTwoLists function made

--- Day6.py
+++ Day6.py
@@ -1,3 +1,11 @@
 # Definition for singly-linked list.
 class ListNode(object):
-   
+    #method definition
+    def createNode(self, val=0, next=None):
+        # Constructor to create a new node with a given value and next node
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+       
