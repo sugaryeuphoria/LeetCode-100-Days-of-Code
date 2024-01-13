5f786443368367f5043f6fa585872828768cb995
Pooja Verma <pooja32verma@gmail.com>
2024-01-12 22:42:56 -0800

    list2 updated

--- Day6.py
+++ Day6.py
@@ -24,6 +24,8 @@ class Solution(object):
                 current.next = list1
                 list1 = list1.next
             else:
-               
+                current.next = list2
+                list2 = list2.next
+           
        
             
