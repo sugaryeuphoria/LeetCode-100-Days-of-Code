521cdd91a3b487b20e7c8cab3abfdaf06c66392a
Pooja Verma <pooja32verma@gmail.com>
2024-01-14 19:22:38 -0800

    Formatted the code

--- Day8.py
+++ Day8.py
@@ -4,8 +4,9 @@ class Solution(object):
     def removeElement(self, nums, val):
        # Check if the list is empty
         if not nums:
-            return 0  # If empty, no elements to remove, return 0
+             # If empty, no elements to remove, return 0
 
+            return 0 
         # Initialize a pointer for non-val elements
         non_val_pointer = 0
 
