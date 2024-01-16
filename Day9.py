58f169c63c2383c4dc25a9fdb2d233413c69c9f5
Pooja Verma <pooja32verma@gmail.com>
2024-01-15 20:55:57 -0800

    Formatted document

--- Day9.py
+++ Day9.py
@@ -13,7 +13,7 @@ class Solution(object):
 
       # Iterate through the haystack
         #Inside the loop, it checks if the substring of haystack (starting from the current index i and having the same length as needle) is equal to needle. 
-        
+        #If there's a match, it means needle is found in haystack, and the function returns the index i.
         for i in range(len(haystack) - len(needle) + 1):
             # Check if the substring matches needle
             if haystack[i:i+len(needle)] == needle:
