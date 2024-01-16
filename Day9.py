287f08b97dcb7bc8f8e18291dfb9aaa84fb36117
Pooja Verma <pooja32verma@gmail.com>
2024-01-15 20:38:00 -0800

    Comments added

--- Day9.py
+++ Day9.py
@@ -2,4 +2,9 @@
 class Solution(object):
     #Method definition
     def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
        
