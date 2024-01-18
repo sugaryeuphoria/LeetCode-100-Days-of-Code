5a5cbae8450eccd9f15dd5e73bcfb9843bcead3d
Pooja Verma <pooja32verma@gmail.com>
2024-01-17 18:33:21 -0800

    Class and method definition

---
+++ Day14.py
@@ -0,0 +1,12 @@
+"""Add Binary
+Easy
+9087
+921
+Given two binary strings a and b, return their sum as a binary string.
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+"""
+class Solution(object):
+    def addBinary(self, a, b):
+       
