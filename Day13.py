328b73a5345cf730d9feaac2bd45754d63e54ebd
Pooja Verma <pooja32verma@gmail.com>
2024-01-17 18:29:19 -0800

    Method definition

--- Day13.py
+++ Day13.py
@@ -3,4 +3,6 @@
 Increment the large integer by one and return the resulting array of digits."""
 #Class Declaration
 class Solution(object):
-   
+    #Method definition
+    def plusOne(self, digits):
+       
