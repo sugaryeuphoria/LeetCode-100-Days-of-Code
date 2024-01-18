8dd1c2750c1028a1f5ee45915afe2ef69c56024a
Pooja Verma <pooja32verma@gmail.com>
2024-01-17 18:23:25 -0800

    Get the length of the last word

--- Day12.py
+++ Day12.py
@@ -2,6 +2,12 @@
 class Solution(object):
     #Method declaration
     def lengthOfLastWord(self, s):
-        # Split the string into words
+       # Split the string into words
         words = s.split()
-      
+       # Check if there are any words
+        if not words:
+            return 0
+       # Get the length of the last word
+        last_word = words[-1]
+        return len(last_word)
+        
