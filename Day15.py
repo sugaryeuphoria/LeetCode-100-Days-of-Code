c62751b6eb933b90fb354f37ee0742a8405b64ed
Pooja Verma <pooja32verma@gmail.com>
2024-01-19 15:03:46 -0800

    Check if mid is a square root

--- Day15.py
+++ Day15.py
@@ -12,4 +12,8 @@ class Solution(object):
             mid = (left + right) // 2
             mid_squared = mid * mid
 
-          
+            # Check if mid is the square root
+            if mid_squared == x:
+                return mid
+           
+      
