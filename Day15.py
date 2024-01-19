4c60bd65996d72988117f34af7206fd5af7ba22b
Pooja Verma <pooja32verma@gmail.com>
2024-01-19 15:05:52 -0800

    Comments Added

--- Day15.py
+++ Day15.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
+        
         # Handle base case
         if x == 0 or x == 1:
             return x
