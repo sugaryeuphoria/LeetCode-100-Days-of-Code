0ddf73ab4a8b3917c671934f71024da32a373121
Pooja Verma <pooja32verma@gmail.com>
2024-01-19 15:02:15 -0800

    Binary search for square root

--- Day15.py
+++ Day15.py
@@ -6,5 +6,6 @@ class Solution(object):
         # Handle base case
         if x == 0 or x == 1:
             return x
-
-      
+  # Binary search for the square root
+        left, right = 0, x
+       
