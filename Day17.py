55d6d9375a02b418ead957360c20637287fb2d7d
Pooja Verma <pooja32verma@gmail.com>
2024-01-21 15:46:52 -0800

    Question added

--- Day17.py
+++ Day17.py
@@ -1,3 +1,4 @@
+#Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
 # Definition for singly-linked list.
 # class ListNode(object):
 class Solution(object):
