0dd0d43b8095703b949718f0db2c1b7243205275
Pooja Verma <pooja32verma@gmail.com>
2024-01-21 15:44:19 -0800

    Check if the current node's value is equal to the next node's value

--- Day17.py
+++ Day17.py
@@ -3,10 +3,12 @@
 class Solution(object):
     #Method definition
      def deleteDuplicates(self, head):
-           # Check if the linked list is empty or has only one node
+        # Check if the linked list is empty or has only one node
         if not head or not head.next:
             return head
          # Start from the head of the linked list
         current = head
-          # Traverse the linked list
+        # Traverse the linked list
         while current and current.next:
+            # Check if the current node's value is equal to the next node's value
+            if current.val == current.next.val:
