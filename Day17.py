caf8177b4a3ee9ca52cbaa9977f7e76229ca654e
Pooja Verma <pooja32verma@gmail.com>
2024-01-21 15:43:09 -0800

    Check if the linked list is empty or has only one node

--- Day17.py
+++ Day17.py
@@ -3,3 +3,6 @@
 class Solution(object):
     #Method definition
      def deleteDuplicates(self, head):
+           # Check if the linked list is empty or has only one node
+        if not head or not head.next:
+            return head
