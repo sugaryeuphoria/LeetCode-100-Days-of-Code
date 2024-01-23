7228bc3f0db7520d5e7eaeada10643e18c1740d3
Pooja Verma <pooja32verma@gmail.com>
2024-01-22 16:48:21 -0800

    Merge from the end to the beginning

--- Day18.py
+++ Day18.py
@@ -9,3 +9,6 @@ class Solution(object):
 
         # Initialize pointer for the end of nums1
         k = m + n - 1
+
+         # Merge from the end to the beginning
+        while i >= 0 and j >= 0:
