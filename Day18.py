c3a58cac8628e56376aca564ed4626f8f8484ca9
Pooja Verma <pooja32verma@gmail.com>
2024-01-22 16:49:07 -0800

    Else condition added

--- Day18.py
+++ Day18.py
@@ -16,3 +16,7 @@ class Solution(object):
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
                 i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
