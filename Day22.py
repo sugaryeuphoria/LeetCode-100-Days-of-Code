6cbf08fd3f08e00a444dde06f2185e63f87af24e
Pooja Verma <pooja32verma@gmail.com>
2024-01-25 13:46:24 -0800

    Helper function to expand around

--- Day22.py
+++ Day22.py
@@ -3,4 +3,9 @@ class Solution(object):
     #Method definition
     def longestPalindrome(self, s):
         #def another method
+        def expand_around_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
       
