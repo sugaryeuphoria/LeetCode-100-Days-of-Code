fab917060425d4a555f40123f1e7201fea3037cc
Pooja Verma <pooja32verma@gmail.com>
2024-01-24 16:00:51 -0800

    Node generation

--- Day20.py
+++ Day20.py
@@ -2,4 +2,12 @@ class TreeNode:
     #Method declaration
     @classmethod
     def create(cls, val=0, left=None, right=None):
+        #Node generation
+        node = cls()
+        node.val = val
+        node.left = left
+        node.right = right
+        return node
+
+
      
