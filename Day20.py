6c43fb16e12e764f7a0b49dc11d4dcf1baa96afe
Pooja Verma <pooja32verma@gmail.com>
2024-01-24 16:10:09 -0800

    Question added

--- Day20.py
+++ Day20.py
@@ -1,3 +1,6 @@
+"""Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value."""
 class TreeNode:
     #Method declaration
     @classmethod
