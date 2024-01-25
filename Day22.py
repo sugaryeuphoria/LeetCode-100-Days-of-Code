c1b78e94b95ac703a7c6ab8906e184e563e6a981
Pooja Verma <pooja32verma@gmail.com>
2024-01-25 13:45:06 -0800

    Method definition

--- Day22.py
+++ Day22.py
@@ -1,12 +1,5 @@
 #Class created
 class Solution(object):
     #Method definition
-   def isSymmetric(self, root):
-       # Helper function to check if two trees are mirrors
-        def isMirror(left, right):
-            # If both are empty, they are mirrors
-            if not left and not right:
-                return True
-            # If one is empty and the other is not, they are not mirrors
-            if not left or not right:
-                return False
+    def longestPalindrome(self, s):
+      
