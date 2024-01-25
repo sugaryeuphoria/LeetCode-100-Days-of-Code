bba688e6342c76d2154aea6ee34b9c2302da1d0f
Pooja Verma <pooja32verma@gmail.com>
2024-01-25 13:47:47 -0800

    Initialize variable

--- Day22.py
+++ Day22.py
@@ -11,4 +11,6 @@ class Solution(object):
          # Base case: if the string has 1 or 0 characters, it's already a palindrome
         if len(s) <= 1:
             return s
+         # Initialize the variable to store the longest palindrome
+        longest_palindrome = ""
       
