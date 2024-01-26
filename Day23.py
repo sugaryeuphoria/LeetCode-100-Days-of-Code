ce90e45fd785498380520b53b8855054a3367ac2
Pooja Verma <pooja32verma@gmail.com>
2024-01-26 15:10:58 -0800

    Class creation for alternate method

--- Day23.py
+++ Day23.py
@@ -12,3 +12,37 @@ class Solution(object):
     def maxDepth(self, root):
         #Return statement
         return 0 if root is None else max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+
+# Alternate method
+    #Class creation 
+class TreeNode(object):
+    @classmethod
+    def create(cls, val=0, left=None, right=None):
+        node = cls()
+        node.val = val
+        node.left = left
+        node.right = right
+        return node
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def calculate_depth(node):
+            if node is None:
+                return 0
+            left_depth = calculate_depth(node.left)
+            right_depth = calculate_depth(node.right)
+            return max(left_depth, right_depth) + 1
+
+        return calculate_depth(root)
+
+# Example usage:
+# node = TreeNode.create(3, TreeNode.create(9), TreeNode.create(20, TreeNode.create(15), TreeNode.create(7)))
+# solution = Solution()
+# depth = solution.maxDepth(node)
+# print(depth)
+    
