33d1348af68e0ff37eaefb39c974b1935a7add45
Pooja Verma <pooja32verma@gmail.com>
2024-01-26 15:07:01 -0800

    Node Creation

--- Day23.py
+++ Day23.py
@@ -2,4 +2,15 @@
 class TreeNode(object):
     #Method defintion
     def __init__(self, val=0, left=None, right=None):
-    
+            #Node Creation
+            self.val = val
+            self.left = left
+            self.right = right
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return 0 if root is None else max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
