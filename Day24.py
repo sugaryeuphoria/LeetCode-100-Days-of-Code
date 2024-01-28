68e640f109570c9912afe4d67b2948c7d894811b
Pooja Verma <pooja32verma@gmail.com>
2024-01-27 20:34:04 -0800

    Set initial values for binary search

--- Day24.py
+++ Day24.py
@@ -6,7 +6,12 @@ class Solution:
         # Get lengths of both arrays
         m, n = len(nums1), len(nums2)
 
-         # Ensure nums1 is the smaller array
+        #Ensure nums1 is the smaller array
         if m > n:
             nums1, nums2 = nums2, nums1
             m, n = n, m
+
+        # Set initial values for binary search
+        i_min, i_max, half_len = 0, m, (m + n + 1) // 2
+
+        while i_min <= i_max:
