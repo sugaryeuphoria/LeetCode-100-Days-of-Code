e80f65f206d19e14e9da1bf2cfbfa87709655f07
Pooja Verma <pooja32verma@gmail.com>
2024-01-27 20:33:41 -0800

    Ensure nums1 is the smaller array

--- Day24.py
+++ Day24.py
@@ -5,3 +5,8 @@ class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
         # Get lengths of both arrays
         m, n = len(nums1), len(nums2)
+
+         # Ensure nums1 is the smaller array
+        if m > n:
+            nums1, nums2 = nums2, nums1
+            m, n = n, m
