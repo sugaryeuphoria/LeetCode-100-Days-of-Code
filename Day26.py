2c0baadc19077878847f4b87992fc717ff2830be
Pooja Verma <pooja32verma@gmail.com>
2024-01-29 16:06:18 -0800

    Added nodes

--- Day26.py
+++ Day26.py
@@ -1,3 +1,7 @@
 """Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree."""
 # Definition for a binary tree node.
 class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
