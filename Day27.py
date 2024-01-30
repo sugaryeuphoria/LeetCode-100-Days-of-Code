2ac3039be0dd5144e099dbeeb5fa60052080e386
Pooja Verma <pooja32verma@gmail.com>
2024-01-29 16:14:58 -0800

    Debug

--- Day27.py
+++ Day27.py
@@ -1,6 +1,5 @@
 """Given a string s, find the length of the longest substring without repeating characters.
-Also appeared in the LeetCode contest
-"""
+Also appeared in the LeetCode contest"""
 class Solution:
     def lengthOfLongestSubstring(self, s):
          # Map to store the last index of each character
@@ -19,7 +18,7 @@ class Solution:
             if s[end] in char_index_map and char_index_map[s[end]] >= start:
         
         # Move the start index to the next position of the repeated character
-                start = char_index_map[s[end]] + 1
+              start = char_index_map[s[end]] + 1
 
         # Update the last index of the character
             char_index_map[s[end]] = end
