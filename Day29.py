40b50a3ebfbf230c049393810c2ea1ee50017adc
Pooja Verma <pooja32verma@gmail.com>
2024-01-31 17:08:22 -0800

    ase case: If the node is None, the height is 0

--- Day29.py
+++ Day29.py
@@ -4,4 +4,6 @@ class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         # Helper function to calculate the height of the tree rooted at 'node'
         def height(node):
-         
+           # Base case: If the node is None, the height is 0
+            if not node:
+                return 0
