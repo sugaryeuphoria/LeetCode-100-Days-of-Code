88e6c7b43d6982820c5592cc59c93f9cf486dac6
Pooja Verma <pooja32verma@gmail.com>
2024-01-31 17:08:00 -0800

    Helper function to calculate the height of the tree rooted at 'node'

--- Day29.py
+++ Day29.py
@@ -2,4 +2,6 @@
 class Solution:
     #Method definition
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-      
+        # Helper function to calculate the height of the tree rooted at 'node'
+        def height(node):
+         
