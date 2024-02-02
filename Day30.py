2f98360af94bdb04fd509d7f954db37e10060e08
Pooja Verma <pooja32verma@gmail.com>
2024-02-01 20:27:21 -0800

    Break if the number goes beyond the high limit

--- Day30.py
+++ Day30.py
@@ -13,3 +13,7 @@ class Solution(object):
             # Check if the current number is within the given range
             if low <= num <= high:
                     result.append(num)  # Add the current number to the result list
+
+             # Break if the number goes beyond the high limit
+            if num > high:  
+                break
