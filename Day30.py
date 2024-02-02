56fb2216ffb972e98978dc6185bf041cd90145d2
Pooja Verma <pooja32verma@gmail.com>
2024-02-01 20:13:59 -0800

    Initialize an empty list to store sequential digits in the given range

--- Day30.py
+++ Day30.py
@@ -1,3 +1,6 @@
 #Class and method definition
 class Solution(object):
     def sequentialDigits(self, low, high):
+        # Initialize an empty list to store sequential digits in the given range
+
+        result = []
