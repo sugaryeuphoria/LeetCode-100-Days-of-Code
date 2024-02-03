c23be4410f0468f63b05000edcc272aaf923bcaa
Pooja Verma <pooja32verma@gmail.com>
2024-02-02 18:41:53 -0800

    Array to store the maximum sum at each index

--- Day31.py
+++ Day31.py
@@ -1,4 +1,7 @@
 #Method and class declaration 
 class Solution(object):
     def maxSumAfterPartitioning(self, arr, k):
-      
+        # Create an array to store the maximum sum at each index
+        max_sum = [0] * len(arr)
+        
+       
