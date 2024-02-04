d68ab3d4e8cd0f0f45909029255c196171ab0488
Pooja Verma <pooja32verma@gmail.com>
2024-02-03 17:32:05 -0800

    Count frequency

--- Day32.py
+++ Day32.py
@@ -3,4 +3,7 @@ from collections import Counter  # Import Counter to efficiently count character
 #Class and method definition
 class Solution(object):
     def minWindow(self, s, t):
-     
+        char_count_t = Counter(t)  # Count the frequencies of characters in string t.
+        required_chars = len(char_count_t)  # Total unique characters required to form the window.
+
+      
