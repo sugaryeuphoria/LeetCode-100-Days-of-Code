bf10b94a68fc7f7a1068d36e6cd3404db77b58ab
Pooja Verma <pooja32verma@gmail.com>
2024-02-03 17:32:40 -0800

    Count of unique characters formed in the current window.

--- Day32.py
+++ Day32.py
@@ -6,4 +6,5 @@ class Solution(object):
         char_count_t = Counter(t)  # Count the frequencies of characters in string t.
         required_chars = len(char_count_t)  # Total unique characters required to form the window.
 
-      left, right = 0, 0  # Pointers to define the current window.
+        left, right = 0, 0  # Pointers to define the current window.
+        formed_chars = 0  # Count of unique characters formed in the current window.
