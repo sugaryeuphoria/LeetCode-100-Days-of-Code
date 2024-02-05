8b3a7bc55ebb6d8087c8ac743ea6cafdc787fa5c
Pooja Verma <pooja32verma@gmail.com>
2024-02-04 17:58:19 -0800

    Second pass: Find the first unique character

--- Day33.py
+++ Day33.py
@@ -11,3 +11,8 @@ class Solution(object):
  # First pass: Count occurrences
         for char in s:
             char_count[char] += 1
+
+ # Second pass: Find the first unique character
+        for i, char in enumerate(s):
+            if char_count[char] == 1:
+                return i         
