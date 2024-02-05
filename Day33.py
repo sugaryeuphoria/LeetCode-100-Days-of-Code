99acd98beabb90f0a088e1e674fa865749bbbc07
Pooja Verma <pooja32verma@gmail.com>
2024-02-04 17:57:32 -0800

    Let's use a defaultdict to count occurrences

--- Day33.py
+++ Day33.py
@@ -4,3 +4,6 @@ from collections import defaultdict
 #Class and method definition
 class Solution(object):
     def firstUniqChar(self, s):
+
+ # Let's use a defaultdict to count occurrences
+        char_count = defaultdict(int)
