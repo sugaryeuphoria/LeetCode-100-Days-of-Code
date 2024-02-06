185c092fcc911d31e7ec7caf31deddce5de6088e
Pooja Verma <pooja32verma@gmail.com>
2024-02-05 18:39:16 -0800

    import deque from collections,method and class definition

--- Day36.py
+++ Day36.py
@@ -3,3 +3,9 @@
 The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
 
 Note: A leaf is a node with no children."""
+#import deque from collections
+from collections import deque
+
+# method and class definition
+class Solution(object):
+    def minDepth(self, root):
