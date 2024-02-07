448dbcb5dadc6c89beba2defcd7e2d7aa477cdbb
Pooja Verma <pooja32verma@gmail.com>
2024-02-06 19:42:42 -0800

    format

--- Day38.py
+++ Day38.py
@@ -4,24 +4,25 @@ You may assume the two numbers do not contain any leading zero, except the numbe
 # Method and class definition
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
+        
         carry = 0
         dummy = ListNode(0)
         curr = dummy
-
+        
         while l1 or l2 or carry:
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
-
+            
             # Calculate sum and carry
             total = val1 + val2 + carry
             carry = total // 10
             sum_digit = total % 10
-
+            
             # Create new node with sum digit
             curr.next = ListNode(sum_digit)
             curr = curr.next
-
-             # Move to next nodes
+            
+            # Move to next nodes
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         
