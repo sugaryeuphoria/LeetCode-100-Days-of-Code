af891b75cb17fef15350b0ec37ecc430f60dc3cd
Pooja Verma <pooja32verma@gmail.com>
2024-02-09 22:21:39 -0800

    Iterate through each index of the string.

--- Day41.py
+++ Day41.py
@@ -8,4 +8,7 @@ A substring is a contiguous sequence of characters within the string.
 class Solution(object):  # Define a class named Solution.
       def countSubstrings(self, s):  # Define a method named countSubstrings within the Solution class, which takes a string 's' as input.
               n = len(s)  # Get the length of the input string.
-               count = 0 # Initialize a 2D array 'dp' to store whether substrings are palindromes or not.
+              count = 0 # Initialize a 2D array 'dp' to store whether substrings are palindromes or not.
+
+                # Single characters are palindrome by default
+        for i in range(n):  # Iterate through each index of the string.
