2b0562df0a1ac822cfa4ec98097903eddec08c77
Pooja Verma <pooja32verma@gmail.com>
2024-02-09 22:20:32 -0800

    Method definition

--- Day41.py
+++ Day41.py
@@ -6,3 +6,4 @@ A string is a palindrome when it reads the same backward as forward.
 A substring is a contiguous sequence of characters within the string.
 """
 class Solution(object):  # Define a class named Solution.
+      def countSubstrings(self, s):  # Define a method named countSubstrings within the Solution class, which takes a string 's' as input.
