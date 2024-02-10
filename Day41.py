d6289a098a8ab62de4a1181909ce4684cd6d6b62
Pooja Verma <pooja32verma@gmail.com>
2024-02-09 22:20:51 -0800

    Get the length of the input string.

--- Day41.py
+++ Day41.py
@@ -7,3 +7,4 @@ A substring is a contiguous sequence of characters within the string.
 """
 class Solution(object):  # Define a class named Solution.
       def countSubstrings(self, s):  # Define a method named countSubstrings within the Solution class, which takes a string 's' as input.
+              n = len(s)  # Get the length of the input string.
