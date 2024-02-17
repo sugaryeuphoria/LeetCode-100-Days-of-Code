9d70d68470669455086361fdd0a807beda5219cd
Pooja Verma <pooja32verma@gmail.com>
2024-02-16 19:38:50 -0800

    If the size of the priority queue exceeds the number of available ladders, we need to use bricks instead of ladders,Use bricks for the smallest difference

--- Day49.java
+++ Day49.java
@@ -54,6 +54,10 @@ class Solution {
             if (diff > 0) {
                 // Add the difference to the priority queue
                 pq.offer(diff);
+                // If the size of the priority queue exceeds the number of available ladders,
+                // we need to use bricks instead of ladders
+                if (pq.size() > ladders) {
+                    bricks -= pq.poll(); // Use bricks for the smallest difference
             }}
     }
 
