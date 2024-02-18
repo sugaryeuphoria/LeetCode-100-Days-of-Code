b087767fc5cba6baa4e41647d7599383dfcd6637
Pooja Verma <pooja32verma@gmail.com>
2024-02-17 20:45:57 -0800

    Initialize a priority queue to store the differences in heights that require bricks

--- Day50.java
+++ Day50.java
@@ -1,6 +1,9 @@
 import java.util.PriorityQueue;
-// Method declaration
+
 class Day50 {
+    // Method declaration
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
+     // Initialize a priority queue to store the differences in heights that require bricks
+     PriorityQueue<Integer> pq = new PriorityQueue<>();
     }
 }
