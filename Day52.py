5279a123d9913414cfaa5e3354f5a022504675db
Pooja Verma <pooja32verma@gmail.com>
2024-02-19 18:45:58 -0800

    Example 2 added

--- Day52.py
+++ Day52.py
@@ -6,4 +6,9 @@ Input: nums = [3,0,1]
 Output: 2
 Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
  
+Example 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
 """
