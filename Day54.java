54ce0855844f4cf95da832ebe8be93d31094cb23
Pooja Verma <pooja32verma@gmail.com>
2024-02-21 18:26:11 -0800

    Examples added

--- Day54.java
+++ Day54.java
@@ -9,5 +9,16 @@ There is exactly one person that satisfies properties 1 and 2.
 You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai trusts the person labeled bi. If a trust relationship does not exist in trust array, then such a trust relationship does not exist.
 
 Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
- 
+ Example 1:
+
+Input: n = 2, trust = [[1,2]]
+Output: 2
+Example 2:
+
+Input: n = 3, trust = [[1,3],[2,3]]
+Output: 3
+Example 3:
+
+Input: n = 3, trust = [[1,3],[2,3],[3,1]]
+Output: -1
 */
