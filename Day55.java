4a34c869efc94aad8a511069e2c969815cf284b5
Pooja Verma <pooja32verma@gmail.com>
2024-02-22 23:34:56 -0800

    Create an array of lists to represent the graph

--- Day55.java
+++ Day55.java
@@ -1,8 +1,11 @@
 // Class definition
+
+import java.util.List;
+
 public class Day55 {
     // Method definition
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-
-    }
-    
+        // Create an array of lists to represent the graph
+        List<Pair<Integer, Integer>>[] graph = new List[n];
+    }  
 }
