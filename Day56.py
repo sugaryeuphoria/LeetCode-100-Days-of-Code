aa25e3397cc4394a3ac9cfcbfb842d9f2c34f259
Pooja Verma <pooja32verma@gmail.com>
2024-02-23 16:52:24 -0800

    If ranks are equal, arbitrarily choose one as parent and increase rank       self.rank[j] += 1

--- Day56.py
+++ Day56.py
@@ -6,14 +6,18 @@ class UnionFind:
 
     def unionByRank(self, u: int, v: int) -> None:
     # Perform union by rank operation between sets containing elements 'u' and 'v'.
-    i = self._find(u)  # Find the root of 'u'
-    j = self._find(v)  # Find the root of 'v'
+        i = self._find(u)  # Find the root of 'u'
+        j = self._find(v)  # Find the root of 'v'
 
-    if i == j:
-      return           # If 'u' and 'v' are already in the same set, no need to union them
-    
-     if self.rank[i] < self.rank[j]:
-      self.id[i] = j   # 
-      
-      elif self.rank[i] > self.rank[j]:
-      self.id[j] = i   # Attach the root of 'v' to the root of 'u' if rank of 'v' is less
+        if i == j:
+            return           # If 'u' and 'v' are already in the same set, no need to union them
+        
+        if self.rank[i] < self.rank[j]:
+            self.id[i] = j   # 
+        
+        elif self.rank[i] > self.rank[j]:
+            self.id[j] = i   # Attach the root of 'v' to the root of 'u' if rank of 'v' is less
+
+        else:
+      self.id[i] = j   # If ranks are equal, arbitrarily choose one as parent and increase rank
+      self.rank[j] += 1
