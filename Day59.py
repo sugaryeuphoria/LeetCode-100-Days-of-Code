727bb0d95b507b42795bdaf8b71bc81944dd2755
Pooja Verma <pooja32verma@gmail.com>
2024-02-26 18:44:19 -0800

    Definition for a binary tree node.

--- Day59.py
+++ Day59.py
@@ -14,3 +14,9 @@ Constraints:
 The number of nodes in the tree is in the range [1, 104].
 -100 <= Node.val <= 100
 """
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
