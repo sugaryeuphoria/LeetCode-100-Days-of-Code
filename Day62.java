ed48e913eeaaa46860f1118452c9a070246656b8
Pooja Verma <pooja32verma@gmail.com>
2024-02-29 23:37:25 -0800

    return "1".repeat(ones - 1) + "0".repeat(zeros) + "1";

--- Day62.java
+++ Day62.java
@@ -40,6 +40,7 @@ class Day62 {
         //  "1".repeat(ones - 1): Generate a string of '1's, one less than the count of '1's in the input string
         // "0".repeat(zeros): Generate a string of '0's equal to the count of '0's in the input string
         // "1": Append a '1' at the end to ensure the resulting binary number is odd
+        return "1".repeat(ones - 1) + "0".repeat(zeros) + "1";
         
     }
 
