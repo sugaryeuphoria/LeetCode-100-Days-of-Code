f0aeeaac6e36dd76c37e5b19a8972a41e51472e4
Pooja Verma <pooja32verma@gmail.com>
2024-02-29 23:30:51 -0800

    Constraints

--- Day62.py
+++ Day62.py
@@ -20,5 +20,9 @@ Input: s = "0101"
 Output: "1001"
 Explanation: One of the '1's must be in the last position. The maximum number that can be made with the remaining digits is "100". So the answer is "1001".
  
+Constraints:
 
+1 <= s.length <= 100
+s consists only of '0' and '1'.
+s contains at least one '1'.
 """
