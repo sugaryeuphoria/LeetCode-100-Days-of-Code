d5bae41d5a1ab3d7f6a4a78b25e44895f60ee6c2
Pooja Verma <pooja32verma@gmail.com>
2024-03-01 18:46:21 -0800

    Follow up

--- Day 63.py
+++ Day 63.py
@@ -17,4 +17,6 @@ Constraints:
 1 <= nums.length <= 104
 -104 <= nums[i] <= 104
 nums is sorted in non-decreasing order.
+
+Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
 """
