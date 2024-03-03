c0e30608597e5aea4a32e1b19fa4de5a256d1d47
Pooja Verma <pooja32verma@gmail.com>
2024-03-02 17:43:34 -0800

    Definition for singly-linked list.

--- Day63.py
+++ Day63.py
@@ -24,3 +24,16 @@ The number of nodes in the list is sz.
 0 <= Node.val <= 100
 1 <= n <= sz
 """
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
