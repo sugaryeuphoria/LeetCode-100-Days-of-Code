aff05304c35994de793dc58cb4ffa3996a95c4b3
Pooja Verma <pooja32verma@gmail.com>
2024-03-06 18:44:00 -0800

    Method and class definition

--- Day67.py
+++ Day67.py
@@ -17,3 +17,6 @@ Constraints:
 The number of nodes in the list is in the range [1, 100].
 1 <= Node.val <= 100
 """
+# Method and class definition
+class Solution(object):
+    def middleNode(self, head):
