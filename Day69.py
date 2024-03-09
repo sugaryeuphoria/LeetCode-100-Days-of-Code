dff6fdd6d4783604876ec04075d977760dfb31b8
Pooja Verma <pooja32verma@gmail.com>
2024-03-08 18:35:59 -0800

    Class and method definition

--- Day69.py
+++ Day69.py
@@ -18,3 +18,6 @@ Constraints:
 1 <= nums1[i], nums2[j] <= 109
 Both nums1 and nums2 are sorted in non-decreasing order.
 """
+# Class and method definition 
+class Solution(object):
+    def getCommon(self, nums1, nums2):
