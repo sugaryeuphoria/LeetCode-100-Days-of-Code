683935ec31b3483c457029a0b6740347e5ac88cf
Pooja Verma <pooja32verma@gmail.com>
2024-03-09 20:29:54 -0800

    Constraints

--- Problem23.py
+++ Problem23.py
@@ -2,6 +2,7 @@
 23. Merge k Sorted Lists
 You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 Merge all the linked-lists into one sorted linked-list and return it.
+
 Example 1:
 Input: lists = [[1,4,5],[1,3,4],[2,6]]
 Output: [1,1,2,3,4,4,5,6]
@@ -22,4 +23,12 @@ Example 3:
 Input: lists = [[]]
 Output: []
  
+Constraints:
+
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-104 <= lists[i][j] <= 104
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 104.
 """
