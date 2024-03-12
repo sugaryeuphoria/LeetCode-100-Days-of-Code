38df34e5320be16faae96466e164941614d7e174
Pooja Verma <pooja32verma@gmail.com>
2024-03-11 20:50:47 -0700

    Class and method definition

--- Day72.py
+++ Day72.py
@@ -21,3 +21,5 @@ Constraints:
 The given linked list will contain between 1 and 1000 nodes.
 Each node in the linked list has -1000 <= node.val <= 1000.
 """
+class Solution(object):
+    def removeZeroSumSublists(self, head):
