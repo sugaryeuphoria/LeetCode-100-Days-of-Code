667768fcc54f1ee8fd81912c9118840697bed43a
Pooja Verma <pooja32verma@gmail.com>
2024-03-13 18:21:01 -0700

    Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
    A subarray is a contiguous part of the array.

--- Day74.py
+++ Day74.py
@@ -1,3 +1,5 @@
 """
 930. Binary Subarrays With Sum
+Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+A subarray is a contiguous part of the array.
 """
