7fab35c7da8f4cf80bad2f4391c167dbdc4f6c7f
Pooja Verma <pooja32verma@gmail.com>
2024-03-16 21:50:55 -0700

    Format

--- Day76.py
+++ Day76.py
@@ -19,18 +19,29 @@ nums[i] is either 0 or 1.
 """
 class Solution(object):
     def findMaxLength(self, nums):
-        hashmap = {0: -1}  # Initialize a hashmap to store the cumulative count of 0s and 1s and their indices
-        max_length = 0  # Initialize a variable to store the maximum length of contiguous subarray
-        count = 0  # Initialize a variable to store the cumulative count of 0s and 1s
-        for i, num in enumerate(nums):  # Iterate through each element and its index in the input list
-            if num == 0:  # If the current element is 0
-                count -= 1  # Decrement the cumulative count
-            else:  # If the current element is 1
-                count += 1  # Increment the cumulative count
-            
-            if count in hashmap:  # If the current cumulative count exists in the hashmap
-                max_length = max(max_length, i - hashmap[count])  # Update the maximum length if needed
-            else:  # If the current cumulative count does not exist in the hashmap
-                hashmap[count] = i  # Add the current cumulative count and its index to the hashmap
-        
-        return max_length  # Return the maximum length of contiguous subarray
+        # Initialize a hashmap to store the cumulative count of 0s and 1s and their indices
+        hashmap = {0: -1}  
+        # Initialize a variable to store the maximum length of contiguous subarray
+        max_length = 0  
+         # Initialize a variable to store the cumulative count of 0s and 1s
+        count = 0 
+         # Iterate through each element and its index in the input list
+        for i, num in enumerate(nums): 
+             # If the current element is 0
+            if num == 0: 
+                 # Decrement the cumulative count
+                count -= 1 
+                # If the current element is 1
+            else:  
+                # Increment the cumulative count
+                count += 1  
+             # If the current cumulative count exists in the hashmap
+            if count in hashmap: 
+                # Update the maximum length if needed
+                max_length = max(max_length, i - hashmap[count])  
+                # If the current cumulative count does not exist in the hashmap
+            else:  
+                # Add the current cumulative count and its index to the hashmap
+                hashmap[count] = i  
+         # Return the maximum length of contiguous subarray
+        return max_length 
