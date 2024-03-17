d7c624c40e5452512a16b0a1efbec5f4d521a878
Pooja Verma <pooja32verma@gmail.com>
2024-03-16 19:13:21 -0700

    Constraints

--- Day77.py
+++ Day77.py
@@ -13,4 +13,13 @@ Example 2:
 Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
 Output: [[1,2],[3,10],[12,16]]
 Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+Constraints:
+
+0 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 105
+intervals is sorted by starti in ascending order.
+newInterval.length == 2
+0 <= start <= end <= 105
 """
