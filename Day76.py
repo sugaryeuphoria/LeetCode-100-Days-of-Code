aaddaa8dbcdd6d149d511d7d54a2252eb96466d8
Pooja Verma <pooja32verma@gmail.com>
2024-03-16 21:53:54 -0700

    Debug

--- Day76.py
+++ Day76.py
@@ -1,6 +1,7 @@
 """
 525. Contiguous Array
-Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+Given a binary array nums, return the maximum length of a contiguous subarray with an equal number
+of 0 and 1.
 
 Example 1:
 Input: nums = [0,1]
@@ -45,3 +46,4 @@ class Solution(object):
                 hashmap[count] = i  
          # Return the maximum length of contiguous subarray
         return max_length 
+    
