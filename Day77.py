60423bffc3bb638c3f00c3bced328d6c5f8a5979
Pooja Verma <pooja32verma@gmail.com>
2024-03-16 19:17:02 -0700

    Check if the new interval has not been inserted yet

--- Day77.py
+++ Day77.py
@@ -31,3 +31,4 @@ class Solution:
             if interval[1] < newInterval[0]:  # If the end of the current interval is before the start of the new interval
                 result.append(interval)  # Append the current interval to the result list
             elif interval[0] > newInterval[1]:  # If the start of the current interval is after the end of the new interval
+                if not inserted:  # Check if the new interval has not been inserted yet
