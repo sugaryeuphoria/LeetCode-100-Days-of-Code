782c553502fccf51a2326c4781948d7fe4dbc40a
Pooja Verma <pooja32verma@gmail.com>
2024-03-23 17:11:41 -0700

    Constraints

--- Day84.py
+++ Day84.py
@@ -15,4 +15,10 @@ Output: 3
 Example 3:
 Input: nums = [3,3,3,3,3]
 Output: 3
+
+Constraints:
+1 <= n <= 105
+nums.length == n + 1
+1 <= nums[i] <= n
+All the integers in nums appear only once except for precisely one integer which appears two or more times.
 """
