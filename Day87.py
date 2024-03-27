8756214c4909d87cf5d81e0c4e642a7bde20e83f
Pooja Verma <pooja32verma@gmail.com>
2024-03-26 20:46:37 -0700

    Description

--- Day87.py
+++ Day87.py
@@ -1,3 +1,4 @@
 """
 713. Subarray Product Less Than K
+Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
 """
