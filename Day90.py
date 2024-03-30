f3a08a92b8ff51d2d03e089917b61d8799aebbf1
Pooja Verma <pooja32verma@gmail.com>
2024-03-29 21:58:29 -0700

    Constraints

--- Day90.py
+++ Day90.py
@@ -14,4 +14,8 @@ Example 2:
 Input: nums = [1,2,1,3,4], k = 3
 Output: 3
 Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
+
+Constraints:
+1 <= nums.length <= 2 * 104
+1 <= nums[i], k <= nums.length
 """
