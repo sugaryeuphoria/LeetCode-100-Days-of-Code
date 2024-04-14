e96cc5ea4b206723b9661d7b3b9983b446196933
Pooja Verma <pooja32verma@gmail.com>
2024-04-13 23:52:58 -0700

    Description

--- Day105.py
+++ Day105.py
@@ -1,4 +1,5 @@
 """
 404. Sum of Left Leaves
-
+Given the root of a binary tree, return the sum of all left leaves.
+A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
 """
