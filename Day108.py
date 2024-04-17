873ff3a4747fdc7839df11041b884b02599ba331
Pooja Verma <pooja32verma@gmail.com>
2024-04-16 23:51:30 -0700

    Examples

--- Day108.py
+++ Day108.py
@@ -5,6 +5,18 @@ Return the lexicographically smallest string that starts at a leaf of this tree 
 As a reminder, any shorter prefix of a string is lexicographically smaller.
 For example, "ab" is lexicographically smaller than "aba".
 A leaf of a node is a node that has no children.
+
+Example 1:
+Input: root = [0,1,2,3,4,3,4]
+Output: "dba"
+
+Example 2:
+Input: root = [25,1,3,1,3,0,2]
+Output: "adz"
+
+Example 3:
+Input: root = [2,2,1,null,1,0,null,0]
+Output: "abc"
 """
 
 class Solution(object):
