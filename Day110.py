b05ae1ee5f9f06a999d26d838715af36efce9c0a
Pooja Verma <pooja32verma@gmail.com>
2024-04-19 01:36:23 -0700

    Explore the island using DFS

--- Day110.py
+++ Day110.py
@@ -48,3 +48,4 @@ class Solution(object):
                             for j in range(len(grid[0])):  # Iterate over columns of the grid
                                 if grid[i][j] == '1':  # If the current cell is land ('1')
   count += 1  # Increment the island count
+dfs(i, j)  # Explore the island using DFS
