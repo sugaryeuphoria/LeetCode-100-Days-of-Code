b7ed63f18138ed3cf536f2d1e1e743064c00208a
Pooja Verma <pooja32verma@gmail.com>
2024-04-20 14:22:06 -0700

    Examples added

--- Day111.py
+++ Day111.py
@@ -8,4 +8,26 @@ To keep the land organized, there are designated rectangular areas of hectares t
 land can be represented by a coordinate system where the top left corner of land is (0, 0) and the bottom right corner of land is (m-1, n-1). Find the coordinates of the top left and bottom right corner of each group of farmland. A group of farmland with a top left corner at (r1, c1) and a bottom right corner at (r2, c2) is represented by the 4-length array [r1, c1, r2, c2].
 
 Return a 2D array containing the 4-length arrays described above for each group of farmland in land. If there are no groups of farmland, return an empty array. You may return the answer in any order.
+
+Example 1:
+
+Input: land = [[1,0,0],[0,1,1],[0,1,1]]
+Output: [[0,0,0,0],[1,1,2,2]]
+Explanation:
+The first group has a top left corner at land[0][0] and a bottom right corner at land[0][0].
+The second group has a top left corner at land[1][1] and a bottom right corner at land[2][2].
+
+Example 2:
+
+Input: land = [[1,1],[1,1]]
+Output: [[0,0,1,1]]
+Explanation:
+The first group has a top left corner at land[0][0] and a bottom right corner at land[1][1].
+
+Example 3:
+
+Input: land = [[0]]
+Output: []
+Explanation:
+There are no groups of farmland.
 """
