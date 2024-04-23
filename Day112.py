5180aafa17a2be38d6e82469116639034ace865a
Pooja Verma <pooja32verma@gmail.com>
2024-04-22 22:13:13 -0700

    Constraints

--- Day112.py
+++ Day112.py
@@ -17,4 +17,13 @@ Explanation: As shown, the height of the tree is 1 when the root is the node wit
 Example 2:
 Input: n = 6, edges = [[3,0],[3,1],[3,2],[3,4],[5,4]]
 Output: [3,4]
+
+Constraints:
+
+1 <= n <= 2 * 104
+edges.length == n - 1
+0 <= ai, bi < n
+ai != bi
+All the pairs (ai, bi) are distinct.
+The given input is guaranteed to be a tree and there will be no repeated edges.
 """
