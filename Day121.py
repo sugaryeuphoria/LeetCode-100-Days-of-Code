677bbc0d90877fa320b9d43d2d572596f0e896ad
Pooja Verma <pooja32verma@gmail.com>
2024-05-01 22:51:16 -0700

    Problem description

--- Day121.py
+++ Day121.py
@@ -1,3 +1,6 @@
 """
 2441. Largest Positive Integer That Exists With Its Negative
+Given an integer array nums that does not contain any zeros, find the largest positive integer k such that -k also exists in the array.
+
+Return the positive integer k. If there is no such integer, return -1.
 """
