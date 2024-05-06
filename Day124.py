7c74516732c700c70b2a2743f0f0fa206be1decf
Pooja Verma <pooja32verma@gmail.com>
2024-05-05 23:36:38 -0700

    Problem description

---
+++ Day124.py
@@ -0,0 +1,8 @@
+"""
+2487. Remove Nodes From Linked List
+You are given the head of a linked list.
+
+Remove every node which has a node with a greater value anywhere to the right side of it.
+
+Return the head of the modified linked list.
+"""
