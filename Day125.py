4433fc089965ed3a1d3f51dc3c41ed0a6abf22ec
Pooja Verma <pooja32verma@gmail.com>
2024-05-06 22:36:49 -0700

    Problem description

--- Day125.py
+++ Day125.py
@@ -1,3 +1,6 @@
 """
 2816. Double a Number Represented as a Linked List
+You are given the head of a non-empty linked list representing a non-negative integer without leading zeroes.
+
+Return the head of the linked list after doubling it.
 """
