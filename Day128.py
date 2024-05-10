67ae1a1d421388c2e0ddb31366b2c8afdfaf1cb8
Pooja Verma <pooja32verma@gmail.com>
2024-05-09 17:52:41 -0700

    Constraints

--- Day128.py
+++ Day128.py
@@ -18,4 +18,13 @@ Example 2:
 
 Input: arr = [1,7], k = 1
 Output: [1,7]
+
+Constraints:
+
+2 <= arr.length <= 1000
+1 <= arr[i] <= 3 * 104
+arr[0] == 1
+arr[i] is a prime number for i > 0.
+All the numbers of arr are unique and sorted in strictly increasing order.
+1 <= k <= arr.length * (arr.length - 1) / 2
 """
