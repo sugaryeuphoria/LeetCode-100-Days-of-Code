b22498d60d80ff64dc4e06e88e0fc3d2695b6a50
Pooja Verma <pooja32verma@gmail.com>
2024-05-15 20:05:35 -0700

    Examples , constraints

--- Day131.py
+++ Day131.py
@@ -14,4 +14,28 @@ Otherwise, evaluate the node's two children and apply the boolean operation of i
 Return the boolean result of evaluating the root node.
 A full binary tree is a binary tree where each node has either 0 or 2 children.
 A leaf node is a node that has zero children.
+
+Example 1:
+
+
+Input: root = [2,1,3,null,null,0,1]
+Output: true
+Explanation: The above diagram illustrates the evaluation process.
+The AND node evaluates to False AND True = False.
+The OR node evaluates to True OR False = True.
+The root node evaluates to True, so we return true.
+
+Example 2:
+
+Input: root = [0]
+Output: false
+Explanation: The root node is a leaf node and it evaluates to false, so we return false.
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+0 <= Node.val <= 3
+Every node has either 0 or 2 children.
+Leaf nodes have a value of 0 or 1.
+Non-leaf nodes have a value of 2 or 3.
 """
