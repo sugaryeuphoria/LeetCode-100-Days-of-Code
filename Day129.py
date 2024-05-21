6afb0f21f39ac1aac8d42651bece28ed0afccd13
Pooja Verma <pooja32verma@gmail.com>
2024-05-20 18:14:47 -0700

    Debug

--- Day129.py
+++ Day129.py
@@ -15,7 +15,7 @@ Example 1:
 
 
 Input: grid = [[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]]
-Output: [[9,9],[8,6]]
+Output: [[9,9],[8,6]]k
 Explanation: The diagram above shows the original matrix and the generated matrix.
 Notice that each value in the generated matrix corresponds to the largest value of a contiguous 3 x 3 matrix in grid.
 Example 2:
