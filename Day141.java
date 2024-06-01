6684cfc1ce8f3afdbd3899a5fae40d772c074545
Pooja Verma <pooja32verma@gmail.com>
2024-05-31 22:58:11 -0700

    ans += Math.abs(s.charAt(i) - s.charAt(i - 1)); return ans;

--- Day141.java
+++ Day141.java
@@ -32,7 +32,9 @@ public class Day141 {
     public int scoreOfString(String s) {
         int ans = 0;
         for (int i = 1; i < s.length(); ++i){
-            
+            ans += Math.abs(s.charAt(i) - s.charAt(i - 1));
+
+            return ans;
         }
     }
     
