85b9414caca8e8f6190dea6ed6749e5bf0ec026c
Pooja Verma <pooja32verma@gmail.com>
2024-05-31 22:55:38 -0700

    Problem added

--- Day141.java
+++ Day141.java
@@ -1,3 +1,7 @@
+/*You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+
+Return the score of s. */
+
 public class Day141 {
     
 }
