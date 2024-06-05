15b6b678c95e2f671b9612246b85b3f9e4b93e1e
Pooja Verma <pooja32verma@gmail.com>
2024-06-04 18:31:04 -0700

    Examples

--- Day145.java
+++ Day145.java
@@ -1,5 +1,16 @@
 /* 1002. Find Common Characters
 Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
+
+Example 1:
+
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+
+Example 2:
+
+Input: words = ["cool","lock","cook"]
+Output: ["c","o"]
+ 
 */
 
 public class Day145 {
