f9cf6f2816d4d1a7a5197f95e8e79067511bf8fd
Pooja Verma <pooja32verma@gmail.com>
2024-06-04 18:30:37 -0700

    Problem description

--- Day145.java
+++ Day145.java
@@ -1,6 +1,5 @@
 /* 1002. Find Common Characters
- * 
- * 
+Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
 */
 
 public class Day145 {
