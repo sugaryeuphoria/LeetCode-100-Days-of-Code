4da191b0c87b831a5294a423b593dc931056cd49
Pooja Verma <pooja32verma@gmail.com>
2024-06-08 19:30:18 -0700

    Problem description

--- Day149.java
+++ Day149.java
@@ -1,5 +1,7 @@
 /*
  974. Subarray Sums Divisible by Kb
+ Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+A subarray is a contiguous part of an array.
  */
 
 public class Day149 {
