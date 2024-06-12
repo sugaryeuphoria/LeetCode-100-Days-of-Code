601236b27acb5ed92e619cabfa78fe48dd351d98
Pooja Verma <pooja32verma@gmail.com>
2024-06-11 17:44:36 -0700

    Examples

--- Day152.java
+++ Day152.java
@@ -5,6 +5,16 @@ Given an array nums with n objects colored red, white, or blue, sort them in-pla
 We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
 
 You must solve this problem without using the library's sort function.
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
  */
 
 public class Day152 {
