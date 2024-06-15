e8cf22a1ed0332dd4d4566fb0d9d4beee2da8869
Pooja Verma <pooja32verma@gmail.com>
2024-06-14 19:28:14 -0700

    Examples

--- Day155.java
+++ Day155.java
@@ -9,4 +9,18 @@ Initially, you have w capital. When you finish a project, you will obtain its pu
 Pick a list of at most k distinct projects from given projects to maximize your final capital, and return the final maximized capital.
 
 The answer is guaranteed to fit in a 32-bit signed integer.
+
+Example 1:
+
+Input: k = 2, w = 0, profits = [1,2,3], capital = [0,1,1]
+Output: 4
+Explanation: Since your initial capital is 0, you can only start the project indexed 0.
+After finishing it you will obtain profit 1 and your capital becomes 1.
+With capital 1, you can either start the project indexed 1 or the project indexed 2.
+Since you can choose at most 2 projects, you need to finish the project indexed 2 to get the maximum capital.
+Therefore, output the final maximized capital, which is 0 + 1 + 3 = 4.
+Example 2:
+
+Input: k = 3, w = 0, profits = [1,2,3], capital = [0,1,2]
+Output: 6
  */
