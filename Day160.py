0a975eb6c8671457daf3cc3c035a540bf3929b10
Pooja Verma <pooja32verma@gmail.com>
2024-06-19 16:25:07 -0700

    Define the mappings of Roman numerals

--- Day160.py
+++ Day160.py
@@ -54,3 +54,9 @@ Constraints:
 """
 class Solution(object):
     def intToRoman(self, num):
+        # Define the mappings of Roman numerals
+        val_to_roman = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+            (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+        ]
