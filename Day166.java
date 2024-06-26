f836daf8f8a75d564e4ec7dcc5a313729d0a504e
Pooja Verma <pooja32verma@gmail.com>
2024-06-25 18:18:01 -0700

    Problem description

--- Day166.java
+++ Day166.java
@@ -1,4 +1,7 @@
-/*1382. Balance a Binary Search Tree */
+/*1382. Balance a Binary Search Tree 
+Given the root of a binary search tree, return a balanced binary search tree with the same node values. If there is more than one answer, return any of them.
+A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1.
+*/
 
 public class Day166 {
     
