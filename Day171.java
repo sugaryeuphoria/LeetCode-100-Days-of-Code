531a783cad1807c4c82477fb51cb45d74c499c3d
Pooja Verma <pooja32verma@gmail.com>
2024-06-30 20:23:48 -0700

    Problem description

--- Day171.java
+++ Day171.java
@@ -1,4 +1,7 @@
-/*1550. Three Consecutive Odds */
+/*1550. Three Consecutive Odds 
+ * Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+ 
+*/
 public class Day171 {
     
 }
