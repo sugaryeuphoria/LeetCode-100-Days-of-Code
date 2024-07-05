aa04c09bdc8e334249f49f2772e29e21a0f5f202
Pooja Verma <pooja32verma@gmail.com>
2024-07-04 20:04:21 -0700

    Calculate min and max distances between critical points

--- Day174.py
+++ Day174.py
@@ -70,3 +70,12 @@ class Solution:
                 index += 1
             if len(critical_points) < 2:
                 return [-1, -1]
+            
+            # Calculate min and max distances between critical points
+            min_distance = float('inf')
+            max_distance = critical_points[-1] - critical_points[0]
+            
+            for i in range(1, len(critical_points)):
+                min_distance = min(min_distance, critical_points[i] - critical_points[i - 1])
+            
+            return [min_distance, max_distance]
