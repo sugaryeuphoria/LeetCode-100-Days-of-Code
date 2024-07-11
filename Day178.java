d11224fed6394d06ea0690c50c36dfedfbe1d5dc
Pooja Verma <pooja32verma@gmail.com>
2024-07-11 16:41:15 -0700

    Helper method to reverse a portion of the character array

--- Day178.java
+++ Day178.java
@@ -28,4 +28,13 @@ public class Day178 {
          return result.toString();
      }
     }
-}
+     // Helper method to reverse a portion of the character array
+     private void reverse(char[] chars, int left, int right) {
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+    }
