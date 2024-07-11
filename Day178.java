08372898bdb5a778e8d8747e8f7982f4639a47e3
Pooja Verma <pooja32verma@gmail.com>
2024-07-11 16:38:15 -0700

    Stack to store the indices of the '(' characters

--- Day178.java
+++ Day178.java
@@ -1,5 +1,9 @@
+import java.util.Stack;
+
 public class Day178 {
     public String reverseParentheses(String s) {
-
+        // Stack to store the indices of the '(' characters
+        Stack<Integer> stack = new Stack<>();
+        char[] chars = s.toCharArray();
     }
 }
