4de55f0b498856a55306926b86b0368a5b4f2254
Pooja Verma <pooja32verma@gmail.com>
2024-07-14 18:51:42 -0700

    Problem description

--- Day180.java
+++ Day180.java
@@ -1,4 +1,12 @@
-/*2196. Create Binary Tree From Descriptions */
+/*2196. Create Binary Tree From Descriptions 
+ * You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+
+If isLefti == 1, then childi is the left child of parenti.
+If isLefti == 0, then childi is the right child of parenti.
+Construct the binary tree described by descriptions and return its root.
+
+The test cases will be generated such that the binary tree is valid.
+*/
 public class Day180 {
     
 }
