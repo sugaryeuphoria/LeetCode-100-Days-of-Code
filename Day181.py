df61bd575d7c8d8f2deb6fe037830a8790b58275
Pooja Verma <pooja32verma@gmail.com>
2024-07-17 21:02:12 -0700

    Description

--- Day181.py
+++ Day181.py
@@ -1,3 +1,6 @@
 """
 1530. Number of Good Leaf Nodes Pairs
+You are given the root of a binary tree and an integer distance. A pair of two different leaf nodes of a binary tree is said to be good if the length of the shortest path between them is less than or equal to distance.
+
+Return the number of good leaf node pairs in the tree.
 """
