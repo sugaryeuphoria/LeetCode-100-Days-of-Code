6379b5b8fcc21692597252c5530934b8a9023afc
Pooja Verma <pooja32verma@gmail.com>
2024-07-24 15:45:44 -0700

    Define the mapping array and the nums array for the second example

--- Day182.py
+++ Day182.py
@@ -51,3 +51,6 @@ mapping = [8,9,4,0,2,1,3,5,7,6]
 nums = [991, 338, 38]
 # Print the sorted array based on mapped values
 print(solution.sortJumbled(mapping, nums))
+# Define the mapping array and the nums array for the second example
+mapping = [0,1,2,3,4,5,6,7,8,9]
+nums = [789, 456, 123]
