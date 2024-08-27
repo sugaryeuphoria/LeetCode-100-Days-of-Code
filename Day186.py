62a4d156bc67b2154c6a1ed30fca594fc85c365f
Pooja Verma <pooja32verma@gmail.com>
2024-08-26 17:09:52 -0700

    Class and methods

--- Day186.py
+++ Day186.py
@@ -38,3 +38,17 @@ a != b
 0 <= succProb[i] <= 1
 There is at most one edge between every two nodes.
 """
+import heapq
+from collections import defaultdict
+
+class Solution(object):
+    def maxProbability(self, n, edges, succProb, start_node, end_node):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type succProb: List[float]
+        :type start_node: int
+        :type end_node: int
+        :rtype: float
+        """
+       
