aad934fffc6e5e090eff80aee028c36d061f6f4d
Pooja Verma <pooja32verma@gmail.com>
2024-11-13 17:45:39 -0800

    while left < right:                 total = nums[i] + nums[left] + nums[right] t]])

--- Day 197.py
+++ Day 197.py
@@ -38,3 +38,10 @@ class Solution:
             # Avoid duplicates for the first element
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
+
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
