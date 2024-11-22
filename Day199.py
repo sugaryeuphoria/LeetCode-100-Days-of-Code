09521bbeaf0d195b7de04f1e47b61f216342187d
Pooja Verma <pooja32verma@gmail.com>
2024-11-21 20:39:37 -0800

    apping digits to their corresponding letters

--- Day199.py
+++ Day199.py
@@ -27,3 +27,11 @@ digits[i] is a digit in the range ['2', '9'].
 """
 class Solution(object):
     def letterCombinations(self, digits):
+        if not digits:
+            return []
+        
+        # Mapping digits to their corresponding letters
+        phone_map = {
+            '2': "abc", '3': "def", '4': "ghi", '5': "jkl", '6': "mno",
+            '7': "pqrs", '8': "tuv", '9': "wxyz"
+        }
