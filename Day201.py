a745a39ba0641d4143205c87eee88692f654796d
Pooja Verma <pooja32verma@gmail.com>
2024-12-02 22:15:54 -0800

    2109. Adding Spaces to a String

---
+++ Day201.py
@@ -0,0 +1,16 @@
+"""
+2109. Adding Spaces to a String
+Medium
+
+722
+
+81
+
+Add to List
+
+Share
+You are given a 0-indexed string s and a 0-indexed integer array spaces that describes the indices in the original string where spaces will be added. Each space should be inserted before the character at the given index.
+
+For example, given s = "EnjoyYourCoffee" and spaces = [5, 9], we place spaces before 'Y' and 'C', which are at indices 5 and 9 respectively. Thus, we obtain "Enjoy Your Coffee".
+Return the modified string after the spaces have been added.
+"""
